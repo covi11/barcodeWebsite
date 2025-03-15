358074891c76b2356de247112721a93972f813dd
Covenant Themba <covi11dethemba@gmail.com>
2025-03-15 22:45:14 +0200

    gugl

--- barcodeWebsite/docs/server/angular-app-manifest.mjs
+++ barcodeWebsite/docs/server/angular-app-manifest.mjs
@@ -7,8 +7,8 @@ export default {
   routes: undefined,
   entryPointToBrowserMapping: {},
   assets: {
-    'index.csr.html': {size: 5109, hash: 'c1fa1c76c7c212f493daaa9d0aaae229904f01602b37d3ccb3b35025e0c1ddda', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 1082, hash: '09bc12ee413e2fd40911e23918b798c12bf1d112847c1181618701c9e604e920', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
-    'styles-RNOKCK3Y.css': {size: 244815, hash: 'drBSQjWO93E', text: () => import('./assets-chunks/styles-RNOKCK3Y_css.mjs').then(m => m.default)}
+    'index.csr.html': {size: 5109, hash: '7432df81cf48038369800aea6ecd73b4c7ecaae2dbae35cb2440292b62eb86cd', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 1082, hash: '69d46bba571c27e7afefc9257e730c5a629a2fece44685bd516e84f8509be796', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'styles-ZLZVAEMD.css': {size: 244815, hash: 'BKIUCPa4G2w', text: () => import('./assets-chunks/styles-ZLZVAEMD_css.mjs').then(m => m.default)}
   },
 };
